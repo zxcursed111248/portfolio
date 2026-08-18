49f15f9da240c9f4da3c88f983c1410ca467154b
zxcursed <babel09@list.ru>
2026-08-18 23:51:08 +0500

    Test projects without images

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -112,20 +112,7 @@ export default function Projects() {
           >
             {/* Image */}
             <div className="relative aspect-[16/10] overflow-hidden bg-zinc-900">
-            <Image
-  src={project.image}
-  alt={project.title}
-  fill
-  sizes="(max-width: 768px) 100vw, 50vw"
-  loading="lazy"
-  className="
-    object-cover
-    transition-transform
-    duration-700
-    ease-out
-    group-hover:scale-105
-  "
-/>
+            <div className="absolute inset-0 bg-zinc-900" />
 
               {/* Dark gradient */}
               <div
